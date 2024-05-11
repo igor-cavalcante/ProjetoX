18f6bdf576795ef99437be262197df9c1b8234a8
igor-cavalcante <igorcavalcantealbuquerque@gmail.com>
2024-05-11 11:27:22 -0300

    importando as routes para nosso server express index.js

--- app/index.js
+++ app/index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const path = require("path");
-
+const routes = require("./routes/routes");
 const app = express();
 const port = 8080;
 
 app.set("view engine", "ejs"); //setando a engine ejs
 app.use(express.static(path.join(__dirname, "public"))); // setando os arquivos estaticos
+app.use(routes);
+
 
 //rota home
-app.get("/app/task", (req, res) => {
-  res.render("index");
-});
+// app.get("/app/task", (req, res) => {
+//   res.render("index");
+// });
 
 app.listen(port, () => console.log(`Rodando server express na port : ${port}`));
