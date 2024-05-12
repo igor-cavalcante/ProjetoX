0e5fac398f17dd43ee225239850f9b2c1ad3892a
igor-cavalcante <igorcavalcantealbuquerque@gmail.com>
2024-05-12 15:47:29 -0300

    Atualizando rota getByID pra receber um metodo como parametro

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -5,7 +5,7 @@ const TaskController = require('../controller/TaskController');
 routes.get("/app/task", TaskController.getALLTask);// Rota para mostrar todas as Task's
 routes.get("/app/task/create", TaskController.showCreateForm); // Rota para exibir o formulário de criação
 routes.post("/app/task/create", TaskController.createTask); // Rota para ativar o end poitn de criar task
-routes.get("/app/getByID/:id", TaskController.getTaskById); // obs : sempre que um parametro é mandado pela rota ele vem em formato de string
+routes.get("/app/getByID/:id/:method", TaskController.getTaskById); // obs : sempre que um parametro é mandado pela rota ele vem em formato de string
 routes.post("/app/updateTask/:id", TaskController.updateOneTask); // Rota para atualizar task
 
 module.exports = routes;
