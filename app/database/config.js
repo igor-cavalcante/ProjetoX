3b94e54829e08962732508c7136876155f5eb2b3
igor-cavalcante <igorcavalcantealbuquerque@gmail.com>
2024-05-23 10:30:54 -0300

    update json vercel

--- app/database/config.js
+++ app/database/config.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const ConnectToDataBase = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://${process.env.MONGODB_NAME}:I09HMPwbPVNAZOwd@projetox.a2ui9up.mongodb.net/?retryWrites=true&w=majority`
+      `mongodb+srv://user:I09HMPwbPVNAZOwd@projetox.a2ui9up.mongodb.net/?retryWrites=true&w=majority`
     );
     console.log("Conexão com o banco ocorreu com sucesso!");
   } catch (error) {
