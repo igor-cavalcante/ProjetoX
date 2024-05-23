70f82c2853ea0ecf944da35bea51692276a67eb4
igor-cavalcante <igorcavalcantealbuquerque@gmail.com>
2024-05-23 11:01:16 -0300

    update index

--- app/index.js
+++ app/index.js
@@ -17,6 +17,7 @@ const app = express();
 const port = 8080;
 
 app.set("view engine", "ejs"); //setando a engine ejs
+
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public"))); // setando os arquivos estaticos
 
